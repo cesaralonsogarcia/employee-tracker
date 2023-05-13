2f6431d20b0c2d5e09aca226a019df1fa065e2a8
Cesar Alonso Garcia <cesarg324@gmail.com>
2023-05-13 11:12:40 -0500

    Add index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ VALUES ("Executive"),
        ("Sales");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("CEO", 250000, 1)
+VALUES ("CEO", 250000, 1),
        ("Accountant", 80000, 2),
        ("Electrical Engineer", 115000, 3),
        ("Mechanical Engineer", 100000, 3),
@@ -16,15 +16,3 @@ VALUES ("CEO", 250000, 1)
        ("Sales Manager", 90000, 5),
        ("Sales Associate", 55000, 5),
        ("Customer Service", 50000, 5);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Dua", "Lipa", 1, null),
-       ("Georgina", "Perez", 2, 1),
-       ("Cesar", "Garcia", 3, 1),
-       ("Kayla", "Bennett", 4, 1),
-       ("Daniel", "Salaises", 5, 1),
-       ("Jacob", "Kearns", 6, 1),
-       ("Stephie", "Harvel", 7, 1),
-       ("Carlo", "Ramirez", 8, 7),
-       ("Taylor", "Swift", 9, 1),
-       ("Aimee", "Groves", 10, 9);
---
+++ db/seeds_employee.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dua", "Lipa", 1, null),
+       ("Georgina", "Perez", 2, 1),
+       ("Cesar", "Garcia", 3, 1),
+       ("Kayla", "Bennett", 4, 1),
+       ("Daniel", "Salaises", 5, 1),
+       ("Jacob", "Kearns", 6, 1),
+       ("Stephie", "Harvel", 7, 1),
+       ("Carlo", "Ramirez", 8, 7),
+       ("Taylor", "Swift", 9, 1),
+       ("Aimee", "Groves", 10, 9);
