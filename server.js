705981f332da63545595c74c53d0b915ecf8e65c
Cesar Alonso Garcia <cesarg324@gmail.com>
2023-05-13 10:16:35 -0500

    Add schema.sql and seeds.sql

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
